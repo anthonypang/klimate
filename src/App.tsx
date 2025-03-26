b0a603ae0928ba96405d1d7f4a684a9ab83e54c1
Anthony Pang <anthonypang99@gmail.com>
2025-03-26 13:37:28 -0400

    add default values to react query to handle stale data

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,12 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: ,
-      gcTime: ,
-      retry: ,
-      refetchOnWindowFocus: ,
-    }
-  }
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      gcTime: 10 * 60 * 1000, // 10 minutes
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
 });
 
 function App() {
